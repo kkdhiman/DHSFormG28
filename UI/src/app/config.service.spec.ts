b44ebb93e2a8f0c5720f56f98a84e48378a6754d
jcaple <james.caple@devtechnology.com>
2017-12-21 14:36:23 -0500

    Fix ConfigService Unit Test

--- UI/src/app/config.service.spec.ts
+++ UI/src/app/config.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ConfigService } from './config.service';
 
 describe('ConfigService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ConfigService]
+      providers: [ConfigService],
+      imports: [HttpClientModule]
     });
   });
 

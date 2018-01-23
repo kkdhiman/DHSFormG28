f3d0f6729aa5739cd14ed337a28ead4f0be9c432
jcaple-devtech <james.caple@devtechnology.com>
2018-01-23 15:45:11 -0500

    Fixing API docker build

--- API/src/routes/Routes.js
+++ API/src/routes/Routes.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var authenticate = require('../controllers/Authenticate');
+const authenticate = require('../controllers/Authenticate');
 
 router.post('/authenticate', authenticate);
 

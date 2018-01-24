f3f5467bb441d5ab240aed606b58670da26b0ca8
jcaple <james.caple@devtechnology.com>
2018-01-24 10:42:47 -0500

    Fixed AuthenticateService Unit Tests

--- UI/src/app/authenticate.service.spec.ts
+++ UI/src/app/authenticate.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { User } from './user';
 import { AuthenticateService } from './authenticate.service';
@@ -8,7 +9,7 @@ describe('AuthenticateService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticateService],
+      providers: [AuthenticateService, { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } } ],
       imports: [HttpClientModule]
     });
   });

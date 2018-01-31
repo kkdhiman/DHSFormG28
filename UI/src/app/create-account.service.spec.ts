610310a6511b36a99cad9d8a646b07d8730ffb7b
jcaple <james.caple@devtechnology.com>
2018-01-31 17:59:48 -0500

    Fix unit test

--- UI/src/app/create-account.service.spec.ts
+++ UI/src/app/create-account.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { CreateAccountService } from './create-account.service';
 
 describe('CreateAccountService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CreateAccountService]
+      providers: [CreateAccountService,  { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } }],
+      imports: [HttpClientModule]
     });
   });
 
